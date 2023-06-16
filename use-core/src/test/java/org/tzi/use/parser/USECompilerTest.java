4357b5c83976fc4449b7aac8c24559d716782ccc
Gil Khais <gil4390@gmail.com>
2023-06-16 14:00:03 +0300

    merge

--- use-core/src/test/java/org/tzi/use/parser/USECompilerTest.java
+++ use-core/src/test/java/org/tzi/use/parser/USECompilerTest.java
@@ -120,7 +120,7 @@ public class USECompilerTest extends TestCase {
     public void testSpecification() {
         Options.explicitVariableDeclarations = false;
 
-        List<File> fileList = getFilesMatchingSuffix(".use", 35);
+        List<File> fileList = getFilesMatchingSuffix(".use", 36);
         // add all the example files which should have no errors
         File[] files = EXAMPLES_PATH.listFiles( new SuffixFileFilter(".use") );
         assertNotNull(files);
@@ -165,7 +165,7 @@ public class USECompilerTest extends TestCase {
     public void testMultiSpecification() {
         Options.explicitVariableDeclarations = false;
 
-        List<File> fileList = getFilesMatchingPrefixWithSuffix("multi",".use", 3,35);
+        List<File> fileList = getFilesMatchingPrefixWithSuffix("multi",".use", 4,36);
         // add all the example files which should have no errors
         File[] files = EXAMPLES_PATH.listFiles( new SuffixFileFilter(".use") );
         assertNotNull(files);
