d8ed57ebf9d6148edf67226f759e53def48f0de7
Gil Khais <gil4390@gmail.com>
2023-08-01 14:30:34 +0300

    began implementation for converting object diagrams from single models into combined diagrams in a multi model

---
+++ use-core/src/main/java/org/tzi/use/uml/sys/MMultiSystem.java
@@ -0,0 +1,56 @@
+package org.tzi.use.uml.sys;
+
+import org.tzi.use.uml.mm.MClass;
+import org.tzi.use.uml.mm.MModel;
+import org.tzi.use.uml.mm.MMultiModel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MMultiSystem {
+
+    private MMultiModel fMultiModel;
+    private Map<String, MSystem> fSystems;
+
+    public MMultiSystem(MMultiModel fMultiModel) {
+        this.fMultiModel = fMultiModel;
+        this.fSystems = new HashMap<>();
+
+        for (MModel model : fMultiModel.models()){
+            MSystem modelSystem = new MSystem(model);
+            fSystems.put(model.name(), modelSystem);
+        }
+
+    }
+
+    public void setModelSystem(String modelName, MSystem system){
+        fSystems.replace(modelName, system);
+    }
+
+    public MSystem toMSystem() throws Exception{
+        MModel convertedModel = fMultiModel.toMModel();
+        MSystem convertedSystem = new MSystem(convertedModel);
+
+        //loop over all the previous systems and add the actions to the new system
+
+        for (MSystem system : fSystems.values()){
+            //iterate the objects
+            for (MObject object : system.state().allObjects()){
+                String clsName = system.model().name() + "_" + object.cls().name();
+                MClass convertedClass = convertedModel.getClass(clsName);
+                String newObjName = system.model().name() + "_" + object.name();
+                convertedSystem.createObject(convertedClass, newObjName);
+            }
+
+            //iterate the links
+            for (MLink link : system.state().allLinks()){
+                //convertedSystem.createLinkObject()
+            }
+
+        }
+
+        return convertedSystem;
+    }
+
+
+}
